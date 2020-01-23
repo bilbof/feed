a58d41a4a1320bf4c18c0c8bc7f634d42efd0c9e
Bill Franklin <b.franklin@outlook.com>
2020-01-23 20:05:37 +0000

    Fixup

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: RACK_ENV=production bundle exec puma -p 3000
+web: bundle exec puma -p 3000
--- config/deploy.rb
+++ config/deploy.rb
@@ -18,7 +18,7 @@ set :app_env, "production"
 
 set :rbenv_type, :user # or :system, depends on your rbenv setup
 set :rbenv_ruby, File.read('.ruby-version').strip
-set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RACK_ENV=production RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all # default value
 
